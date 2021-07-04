4c19e829ae211a682f478da710563770f2295d93
noguchi-no <57585688+noguchi-no@users.noreply.github.com>
2021-07-04 20:53:33 +0900

    Revert "リファクタリング、デバッグ"

---
+++ Assets/Script/HairController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairController : MonoBehaviour
+{
+    bool once = true;
+    Hair hair;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hair = GameObject.Find("Hair").GetComponent<Hair>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(transform.position.x <= -5.0f)
+        {
+            if (once)
+            {
+            hair.hairList.Clear();
+            once = false;
+            }
+            
+        }*/
+
+
+    }
+}
