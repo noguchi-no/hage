e88914baef267226d0474d0a3d2df7ca49e2b39d
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2021-07-27 19:26:47 +0900

    リファクタリング、デバッグ

--- Assets/Scripts/HairManager.cs
+++ Assets/Scripts/HairManager.cs
@@ -1,92 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 //髪の毛を管理するスクリプト
 public class HairManager : MonoBehaviour
 {
     public GameObject hairPrefab;
     public static List<GameObject> hairList = new List<GameObject>();
-    public bool hasGeneratedHair = false;
-
+    public bool hasGeneratedHair;
     public GameObject countNumber;
-    int count = 0;
-    //float gameOverTime = 1.5f;
-
-    //毛を全部消したか格納する
-    public bool isClear = false;
-
-
-    public GameManager gameManager;
-    
+    public GameManager gameManager;    
     
     // Update is called once per frame
     void Update()
     {
-
         //髪の毛が生成されていないなら
-        if (!hasGeneratedHair)
-        {
-
-            //ボタンが押されたら
-            if (Input.GetMouseButtonDown(0))
-            {   
-                    //gameOverTime = 1.5f;
+        if (!hasGeneratedHair && gameManager.hasGeneratedHagePic) {
                     
-                    int hairCount = (int)Random.Range(1, 4.99f);
-                
-                    for(int i = 0; i < hairCount; i++)
-                    {           
-                        float hairPosition = Random.Range(6.0f, 8.0f);
+            int hairCount = (int)Random.Range(1, 4.99f);
+        
+            for(int i = 0; i < hairCount; i++) {
 
-                        GameObject hair = Instantiate(hairPrefab, new Vector3(hairPosition,3.0f,0f), Quaternion.identity);
+                float hairPosition = Random.Range(6.0f, 8.0f);
 
-                        hairList.Add(hair);
+                GameObject hair = Instantiate(hairPrefab, new Vector3(hairPosition,3.0f,0f), Quaternion.identity);
 
-                        hair.transform.SetParent(gameManager.currentPicture.transform);
+                hairList.Add(hair);
 
-                    } 
+                hair.transform.SetParent(gameManager.currentPicture.transform);
 
-                    hasGeneratedHair = true;   
-                    isClear = false;          
-            }
+            } 
+
+            hasGeneratedHair = true;   
            
-        }
-        else
-        {
+        } else {
 
             //ボタンが押されたら
-            if(Input.GetMouseButtonDown(0))
-            {
-                
+            if(Input.GetMouseButtonDown(0)) {
 
-                if(hairList.Count == 0)
-                {
-                    hasGeneratedHair = false;
-                    isClear = true;
-                    
-                    /*Invoke("Empty", 1.5f);
-                    
-                    
-                    if(gameOverTime > 0.0f && gameOverTime < 1.5f)
-                    {
-                        Debug.Log("GameOver");
-                    }
+                if(hairList.Count > 0) {
 
-                    gameOverTime -= Time.deltaTime;
-
-                    if(gameOverTime <= 0.0f)
-                    {
-                        gameOverTime = 0.0f;
-                    }*/
-
-                }
-                
-                    
-                else
-                {
-                    count++;
+                    gameManager.score++;
                     Destroy(hairList[hairList.Count - 1]);
 
                     //リストからも消す
@@ -96,17 +50,21 @@ public class HairManager : MonoBehaviour
 
             }
 
+            if(hairList.Count == 0) {
+
+                if(gameManager.currentPicture != null) {
+
+                    if(Input.GetMouseButtonUp(0)) {
+
+                        gameManager.currentPicture.GetComponent<HagePicture>().isClear = true;
+
+                    }
+                                    
+                }
+
+            }
+
         }
-
-        countNumber.GetComponent<Text>().text = count.ToString();
-
-        
           
     }
-    /*void Empty()
-        {
-            hasGeneratedHair = false;
-            isClear = true; 
-        }
-        */
 }
