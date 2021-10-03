ad14eb5965f0729ecad6da91dfe6cc89a46537b9
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2021-10-03 14:27:35 +0900

    シェアするボタン修正

--- Assets/Scripts/ShareButton.cs
+++ Assets/Scripts/ShareButton.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.IO;
+using System;
+
 public class ShareButton : MonoBehaviour {
 
     string imagePath {
@@ -10,12 +14,30 @@ public class ShareButton : MonoBehaviour {
         }
     }
 
-
     public void Click() {
+        StartCoroutine(_Share());
+    }
+    public IEnumerator _Share() {
+
+        File.Delete(imagePath);
+        // 削除が完了するまで待機
+        while (true) {
+            if (!File.Exists(imagePath)) break;
+            yield return null;
+        }
+
 
         ScreenCapture.CaptureScreenshot("image.png");
+        // 撮影画像の書き込みが完了するまで待機
+        while (true) {
+            if (File.Exists(imagePath)) break;
+            yield return null;
+        }
+        // 撮影画像の保存処理のため、１フレーム待機
+        yield return new WaitForEndOfFrame();
 
-        SocialConnector.SocialConnector.Share("暇つぶしスマホアプリ「ハーゲンタップ」で遊んでるよ！！", "https://apple.co/3kWEWuQ", imagePath);
+        string text = "暇つぶしスマホアプリ「ハーゲンタップ」で遊んでるよ！！";
+
+        SocialConnector.SocialConnector.Share(text, "https://apple.co/3kWEWuQ", imagePath);
     }
 }
-
